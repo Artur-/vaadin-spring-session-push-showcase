73857c2e9356847e070b926123f6bbcbc174d291
Karsten Hauser <kl.hauser@epages.com>
2017-02-18 17:10:39 +0100

    inital commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.vaadin.spring.session.redis.VaadinSessionRewriteFilter;
+
+@SpringBootApplication
+@EnableRedisHttpSession
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public Filter vaadinSessionRewriteFilter() {
+        return new VaadinSessionRewriteFilter();
+    }
+
+}
---
+++ src/main/java/com/example/VaadinUI.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import com.vaadin.annotations.Push;
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.server.communication.PushConnection;
+import com.vaadin.shared.ui.ui.Transport;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.UIDetachedException;
+import com.vaadin.ui.themes.ValoTheme;
+
+@SpringUI
+@Theme(ValoTheme.THEME_NAME)
+@Push(transport = Transport.LONG_POLLING)
+public class VaadinUI extends UI {
+
+    private static final long serialVersionUID = 8135679809390061654L;
+
+    private HorizontalLayout layout = new HorizontalLayout();
+
+    @Override
+    protected void init(VaadinRequest request) {
+        layout.addComponent(new Button("Test", e -> e.getButton().setCaption("Ok")));
+        setContent(layout);
+
+        buildPushSomething();
+    }
+
+
+    private void buildPushSomething() {
+        class FeederThread extends Thread {
+            @Override
+            public void run() {
+                try {
+
+                    getUI().access(() -> {
+                        layout.addComponent(new Label("Hallo"));
+                    });
+                    Thread.sleep(10000);
+                    getUI().access(() -> {
+                        layout.addComponent(new Label("Du"));
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        new FeederThread().start();
+    }
+
+}
