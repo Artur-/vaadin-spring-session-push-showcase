63a0d9ccdf89df870304b74a5e72f8de05b3ecd1
Karsten Hauser <kl.hauser@epages.com>
2017-04-27 18:01:35 +0200

    working sample (except push) without error messages

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,11 +3,10 @@ package com.example;
 import org.atmosphere.cpr.AtmosphereResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
+@EnableRedisHttpSession
 public class DemoApplication {
 
     public static AtmosphereResource storedResource;
